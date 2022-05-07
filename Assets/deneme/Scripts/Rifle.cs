67b88e10b848da709d27212d5ec8c3bf643a5059
jokeris727 <bariscelik727@hotmail.com>
2022-05-07 14:32:24 +0300

    controller update
    controller update

--- Assets/deneme/Scripts/Rifle.cs
+++ Assets/deneme/Scripts/Rifle.cs
@@ -8,21 +8,86 @@ public class Rifle : MonoBehaviour
     public Camera cam;
     public float giveDamageOf = 10f;
     public float shootingRange = 100f;
+    public float fireCharge = 15f;
+    private float nextTimeToShoot = 0f;
+    public Animator animator;
+    public PlayerScript player;
+    public Transform hand;
+
+    [Header("Riffle Ammo and Shooting")]
+    private int maximumAmmo = 32;
+    public int mag = 10;
+    private int presentAmmo;
+    public float relodingTime = 1.3f;
+    private bool setReloading = false;
 
     [Header("Rifle Effects")]
     public ParticleSystem muzzleSpark;
     public GameObject WoodedEffect;
 
+    private void Awake()
+    {
+        transform.SetParent(hand);
+        presentAmmo = maximumAmmo;
+    }
+
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (setReloading)
+            return;
+
+        if(presentAmmo <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToShoot)
+        {
+            animator.SetBool("Fire", true);
+            animator.SetBool("Idle", false);
+            nextTimeToShoot = Time.time + 1f / fireCharge;
             Shoot();
+        }
+        else if (Input.GetButtonDown("Fire1") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("FireWalk", true);
+        }
+        else if(Input.GetButtonDown("Fire2") && Input.GetButtonDown("Fire1"))
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("IdleAim", true);
+            animator.SetBool("FireWalk", true);
+            animator.SetBool("Walk", true);
+            animator.SetBool("Reloading", false);
+        }
+        else
+        {
+            animator.SetBool("Fire", false);
+            animator.SetBool("Idle", true);
+            animator.SetBool("FireWalk", false);
         }
     }
 
     private void Shoot()
     {
+        //check for mag
+        if(mag == 0)
+        {
+            //show ammo out text
+            return;
+        }
+
+        presentAmmo--;
+
+        if(presentAmmo == 0)
+        {
+            mag--;
+        }
+
+        //updating the UI
+
         muzzleSpark.Play();
         RaycastHit hitInfo;
 
@@ -40,5 +105,21 @@ public class Rifle : MonoBehaviour
             }
 
         }
+    }
+
+    IEnumerator Reload()
+    {
+        player.playerSpeed = 0f;
+        player.playerSprint = 0f;
+        setReloading = true;
+        Debug.Log("Reloading...");
+        animator.SetBool("Reloading", true);
+        //play reload sound
+        yield return new WaitForSeconds(relodingTime);
+        animator.SetBool("Reloading", false);
+        presentAmmo = maximumAmmo;
+        player.playerSpeed = 1.9f;
+        player.playerSprint = 3;
+        setReloading = false;
     }
 }
